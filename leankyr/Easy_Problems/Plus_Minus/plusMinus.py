4b896fae1155d0642c6230721dbec92370b48842
leankyr <leankyr@gmail.com>
2018-10-18 23:27:08 +0300

    remove unused libs

--- leankyr/Easy_Problems/Plus_Minus/plusMinus.py
+++ leankyr/Easy_Problems/Plus_Minus/plusMinus.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
-from functools import reduce
-import math
-import os
-import re
-import sys
-import logging
-#import ipdb
-
 
 def plusMinusReduced(l):
     n = len([ num for num in l if num < 0])
@@ -15,7 +7,6 @@ def plusMinusReduced(l):
     p = len([ num for num in l if num > 0])
     return  n/len(l), z/len(l) , p/len(l)
 
-logging.basicConfig(level=logging.WARN, format='%(levelname)s- %(message)s')
 if __name__ == '__main__':
     with open("testcases/testcase0.txt") as file_obj:
         data = file_obj.readlines()
