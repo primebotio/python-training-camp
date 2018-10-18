64f1a8ac69bb3aec95546c15848285a210468e53
leankyr <leankyr@gmail.com>
2018-10-18 23:01:00 +0300

    v2.0 used list comprehensions

--- leankyr/Easy_Problems/Plus_Minus/plusMinus.py
+++ leankyr/Easy_Problems/Plus_Minus/plusMinus.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
-
+from functools import reduce
 import math
 import os
 import re
@@ -8,22 +8,12 @@ import sys
 import logging
 #import ipdb
 
-def plusMinus(m):
-   # sz = len(m)
-    n=0
-    z=0
-    p=0
-    for i in range(len(m)):
-        if m[i] < 0 :
-            n += 1
-        elif m[i] == 0:
-            z += 1
-        else:
-            p += 1
 
-    return  n/len(m) , z/len(m) , p/len(m)
-
-
+def plusMinusReduced(l):
+    n = len([ num for num in l if num < 0])
+    z = len([ num for num in l if num == 0])
+    p = len([ num for num in l if num > 0])
+    return  n/len(l), z/len(l) , p/len(l)
 
 logging.basicConfig(level=logging.WARN, format='%(levelname)s- %(message)s')
 if __name__ == '__main__':
@@ -32,7 +22,7 @@ if __name__ == '__main__':
     n = int(data[0])
     arr = list(map(int, data[1].rstrip().split()))
     
-    n,z,p = plusMinus(arr)
+    n,z,p = plusMinusReduced(arr)
     
     print ("%6f" %p)
     print ("%6f" %n)
