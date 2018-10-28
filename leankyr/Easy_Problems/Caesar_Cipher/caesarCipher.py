135c442a2ac02955b5a073d36d93cb76ae8b525d
leankyr <leankyr@gmail.com>
2018-10-28 19:32:59 +0200

    Improve Readability Of Caesar Cipher

--- leankyr/Easy_Problems/Caesar_Cipher/caesarCipher.py
+++ leankyr/Easy_Problems/Caesar_Cipher/caesarCipher.py
@@ -4,40 +4,33 @@
 import sys
 
 def caesarCipher(s, k):
-    crypts = ''
     k = k%26
-    for c in s:
-        ### HOTFIX ####
-        if c.isupper() and ord(c) == 90 - k:
-            crypts += 'Z'
-            continue
-        if c.islower() and ord(c) == 122 - k:
-            crypts += 'z'
-            continue
-        ### END HOTFIX ###
-        
-        
-        
-        if ord(c) in range(ord('A'),ord('Z')+1):
-            c = chr((ord(c) + k)%ord('Z'))
-            if ord(c) < ord('A'):
-                c = chr(ord(c) + ord('A')-1)
-            crypts += c
-        elif ord(c) in range(ord('a'),ord('z')+1):
-            c = chr((ord(c) + k)%ord('z'))
-            if ord(c) < ord('a'):
-                c = chr(ord(c) + ord('a')-1)
-            crypts += c
-        else:
-            crypts += c
-            continue
-    return crypts
+    # L2I Mappin
+    L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(0,26)))
+    I2L = dict(zip(range(0,26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+    
+    l2i = dict(zip("abcdefghijklmnopqrstuvwxyz",range(0,26)))
+    i2l = dict(zip(range(0,26),"abcdefghijklmnopqrstuvwxyz"))
+    
+    # print (L2I['z'])
+    # print (I2L[52])
 
+    encrypted_str = ''
+    for c in s:
+        if c in L2I:
+            c = I2L[(L2I[c] + k)%26]
+        elif c in l2i:
+            c = i2l[(l2i[c] + k)%26]
+        else:
+            pass
+        encrypted_str += c
+
+    return encrypted_str
 
 
 
 if __name__ == '__main__':
-    with open("testcases/testcase2.txt") as file_obj:
+    with open("testcases/testcase0.txt") as file_obj:
         data = file_obj.readlines()
 
     n = int(data[0])
