3fdf5f1689933bcf1d052cd828bc37c19f927a76
leankyr <leankyr@gmail.com>
2018-11-01 22:22:40 +0200

    first Effort on GOT sometimes gets timed out

---
+++ leankyr/Easy_Problems/Game_Of_Thrones/gameOfThrones.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def gameOfThrones(s):
+    #s.strip('\n')
+    counter = 0
+    st = set()
+    for c in s:
+        print (s.count(c))
+        if s.count(c) % 2 != 0 and c not in st:
+            st.add(c)
+            counter += 1
+        if counter == 3:
+            return False
+
+    print (counter)
+    return True
+
+
+
+
+if __name__ == '__main__':
+    with open("testcases/testcase2.txt") as file_obj:
+        data = file_obj.readlines()
+    s = data[0]
+    print (s)
+
+    result = gameOfThrones(s)
+
+    if result:
+        print('Yes')
+    else:
+        print('No')
+
+
+
+
+
+
+
+
+
+
+
