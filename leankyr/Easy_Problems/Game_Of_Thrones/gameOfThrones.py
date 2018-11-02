20d6080439a9ec8e31b19161d49ce66caededd25
leankyr <leankyr@gmail.com>
2018-11-02 17:52:26 +0200

    Trying to drop complexity

--- leankyr/Easy_Problems/Game_Of_Thrones/gameOfThrones.py
+++ leankyr/Easy_Problems/Game_Of_Thrones/gameOfThrones.py
@@ -9,8 +9,6 @@ import sys
 
 
 
-NO_OF_CHARS = 256
-
 def gameOfThrones(s):
     s = s.rstrip('\n')
     counter = 0
@@ -29,30 +27,6 @@ def gameOfThrones(s):
     return True
 
 
-def canFormPalindrome(st) :
-
-    # Create a count array and initialize   
-    # all values as 0 
-    count = [0] * (NO_OF_CHARS)
-
-    # For each character in input strings, 
-    # increment count in the corresponding 
-    # count array 
-    for i in range( 0, len(st)) :
-        count[ord(st[i])] = count[ord(st[i])] + 1
-
-    # Count odd occurring characters 
-    odd = 0
-
-    for i in range(0, NO_OF_CHARS ) :
-        if (count[i] & 1) :
-            odd = odd + 1
-
-        if (odd > 1):
-            return False
-
-    # Return true if odd count is 0 or 1,  
-    return True
 
 
 
@@ -63,7 +37,7 @@ if __name__ == '__main__':
     s = data[0]
     # print (s)
 
-    result = canFormPalindrome(s)
+    result = gameOfThrones(s)
 
     if result:
         print('Yes')
