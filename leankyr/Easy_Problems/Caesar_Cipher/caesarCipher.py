0d0835a4b3bb7a1970bd84b25724b9498a1228c9
leankyr <leankyr@gmail.com>
2018-11-04 14:13:34 +0200

    Implemented Caesar Cipher using deques

--- leankyr/Easy_Problems/Caesar_Cipher/caesarCipher.py
+++ leankyr/Easy_Problems/Caesar_Cipher/caesarCipher.py
@@ -2,40 +2,47 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from collections import deque
 
-def caesarCipher(s, k):
-    k = k%26
-    # Caps2integers Mappin
-    Caps2integers = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(0,26)))
-    integers2Caps = dict(zip(range(0,26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-    
-    letters2integers = dict(zip("abcdefghijklmnopqrstuvwxyz",range(0,26)))
-    integers2letters = dict(zip(range(0,26),"abcdefghijklmnopqrstuvwxyz"))
-    
-    # print (L2I['z'])
-    # print (I2L[52])
+def caesar_cipher(s, k):
+    capitals_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase_alphabet = capitals_alphabet.lower()
 
-    encrypted_str = ''
-    for char in s:
-        if char in Caps2integers:
-            char = integers2Caps[(Caps2integers[char] + k)%26]
-        elif char in letters2integers:
-            char = integers2letters[(letters2integers[char] + k)%26]
+    #alphabet = capitals_alphabet + lowercase_alphabet
+
+    # deque takes as argument the think I want to turn into deque
+    encrypted_caps = deque(capitals_alphabet)
+    encrypted_caps.rotate(-k)
+
+    encrypted_lower = deque(lowercase_alphabet)
+    encrypted_lower.rotate(-k)
+
+    # When I "add" two lists like strings it appends the second to the right of the first
+    keys = list(capitals_alphabet) + list(lowercase_alphabet)
+    # print (keys)
+    vals = list(encrypted_caps) + list(encrypted_lower)
+    # print (vals)
+    lettermap = dict(zip(keys,vals))
+
+    encrypted_string = ''
+
+    for c in s:
+        if c in lettermap: # or in lettermap.keys()
+            encrypted_string += lettermap[c]
         else:
-            pass
-        encrypted_str += char
+            encrypted_string += c
 
-    return encrypted_str
+    return encrypted_string
 
 
 
 if __name__ == '__main__':
-    with open("testcases/testcase2.txt") as file_obj:
+    with open("testcases/testcase0.txt") as file_obj:
         data = file_obj.readlines()
 
     n = int(data[0])
     s = data[1]
     k = int(data[2])
 
-    print(caesarCipher(s, k))
+    print(caesar_cipher(s, k))
 
