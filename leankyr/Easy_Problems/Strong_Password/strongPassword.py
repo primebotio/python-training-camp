3ab2aa8c4cf3decb6a286cf29d721eed4251f036
leankyr <leankyr@gmail.com>
2018-11-06 20:02:27 +0200

    Implemented Strong Password the noob way

--- leankyr/Easy_Problems/Strong_Password/strongPassword.py
+++ leankyr/Easy_Problems/Strong_Password/strongPassword.py
@@ -14,14 +14,43 @@ def minimum_number(n, password):
     upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     special_characters = "!@#$%^&*()-+"
 
-    return
+
+    stuff_to_add = 4
+
+    for c in password:
+        if c in lower_case:
+            stuff_to_add -= 1
+            break
+
+    for c in password:
+        if c in upper_case:
+            stuff_to_add -= 1
+            break
+
+    for c in password:
+        if c in numbers:
+            stuff_to_add -= 1
+            break
+
+    for c in password:
+        if c in special_characters:
+            stuff_to_add -= 1
+            break
+
+    print (stuff_to_add)
+
+    if n + stuff_to_add >= 6:
+        return stuff_to_add
+    else:
+        return 6 - n
+
 
 
 
 
 
 if __name__ == '__main__':
-    with open("testcases/testcase1.txt") as file_obj:
+    with open("testcases/testcase0.txt") as file_obj:
         data = file_obj.readlines()
 
     n = int(data[0])
