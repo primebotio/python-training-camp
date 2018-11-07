1d3ff6f19cbda95a4c542da3586d9845ad90b1e7
leankyr <leankyr@gmail.com>
2018-11-08 01:45:07 +0200

    initial commit

---
+++ leankyr/Easy_Problems/Davis_Staircase/davisStaircase.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+import sys
+
+from collections import Counter
+
+
+
+
+
+def stepPerms(n):
+    return
+
+
+
+
+if __name__ == '__main__':
+    with open("testcases/testcase0.txt") as file_obj:
+        data = file_obj.readlines()
+    s = int(data[0])
+    # print (s)
+
+
+    for i in range(s):
+        n = int(data[i+1])
+        # print (n)
+        res = stepPerms(n)
+        print (res)
+
+
+
+
+
+
+
+
+
+
